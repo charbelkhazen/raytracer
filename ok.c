13c33284ac0ac5965c8f16b309e38e09a9a2e57e
charbel <khazencharbel@gmail.com>
2026-01-02 14:48:47 +0200

    added function for cmp double

--- ok.c
+++ ok.c
@@ -1,11 +1,13 @@
-#include "stdlib.h" 
-#include <stdio.h>
-
-int std_pow(int b, int p)
+#include <math.h>
+int	std_cmpDouble(double a, double b, double eps)
 {
-	int res;
-
-	if (!p)
+	if (fabs(a - b) < eps)
 		return (1);
-	return	b * std_pow(b, p - 1);
+	return (0);
+}
+
+#include <stdio.h>
+int main()
+{
+	printf("%d\n", std_cmpDouble(0.1 + 0.2, 0.3, 1e-9));
 }
--- stdlib.c
+++ stdlib.c
@@ -1,18 +1,5 @@
 #include <unistd.h>
 #include <signal.h>
-#define std_assert(expr) (expr ? (void)0 : std_assertfail(#expr)) // subtle -> macro because permits dealing with expr as int and str
-
-#include <stdio.h> // del del del FOR DEBUG
-//only for p > 0
-long int std_pow(int b, int p)
-{
-	std_assert(p > 0);
-	int res;
-
-	if (!p)
-		return (1);
-	return	(b * std_pow(b, p - 1));
-}
 
 size_t	std_strlen(const char *str)
 {
@@ -23,12 +10,23 @@ size_t	std_strlen(const char *str)
 		len++;
 	return (len);
 }
-
 void std_assertfail(const char *expr)
 {
 	write(2, "Assertion Failure: ", 19); 
 	write(2, expr, std_strlen(expr)); //does not respect no io condition
 	raise(SIGABRT);//change to exit before eval and remove header
+}
+#define std_assert(expr) (expr ? (void)0 : std_assertfail(#expr)) // subtle -> macro because permits dealing with expr as int and str
+
+#include <stdio.h> // del del del FOR DEBUG
+//only for p > 0
+long int std_pow(int b, int p)
+{
+	int res;
+
+	if (!p)
+		return (1);
+	return	(b * std_pow(b, p - 1));
 }
 
 static int	getIntSize(int n)
@@ -109,8 +107,7 @@ int	std_dtoa(char *arr, double n)
 int main()
 {
 	
-	printf("%d\n", std_extract_frac(12.45));
+	//printf("%d\n", std_extract_frac(12.45));
 	printf("%ld\n",std_pow(10, 16));
 	printf("%ld\n",std_pow(10, 16));
 }
-
