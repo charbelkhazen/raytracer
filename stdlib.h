02e7da65dc2f5a4a42a7af865a0f4df8b886b539
charbel <khazencharbel@gmail.com>
2026-01-03 12:03:02 +0200

    clean stdlib interface

--- stdlib.h
+++ stdlib.h
@@ -1,36 +1,15 @@
 int	std_pow(int base, int power);
+
 void	std_assert(int condition);
+
 int	std_strlen(const char *string);
-//must be used with std_itoa
-int	std_sizeArray(int int_from_which_you_form_array);
-/*arr points at at least std_sizeArray(int number) bytes*/
-void	std_itoa(char *array, int number);
+
 int	std_cmpDouble(double double1, double double2, double epsilon);
-//must be used with std_dtoa
-int	std_sizeArrayDouble(double double_from_which_you_form_array, int precision);
+
+/*arr points at at least std_sizeArray(int number) bytes*/
+int	std_sizeArray(int int_from_which_you_form_array);
+void	std_itoa(char *array, int number);
+
 /*arr points at at least std_sizeArrayDouble(int number, precision) bytes*/
+int	std_sizeArrayDouble(double double_from_which_you_form_array, int precision);
 void	std_dtoa(char *arr, double n, int precision);
-
-
-/*
-#include <stdio.h>
-int main()
-{
-	int	size;
-
-	char arr[size];
-	std_itoa(arr, -12);
-	printf("%s\n", arr);
-}
-*/
-/*
-int main()
-{
-	int	size = std_sizeArrayDouble(10.23, 1);
-	printf("size:%d\n", size);
-	char	arr[100];
-
-	std_dtoa(arr, -10.23, 1);
-	printf("whole thing:%s\n", arr);
-}
-*/
