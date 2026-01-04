ca17e9dbda4ac7438f42ee4cdbe6742290141b9f
charbel <khazencharbel@gmail.com>
2026-01-04 13:54:46 +0200

    ray.c runs

---
+++ ray.c
@@ -0,0 +1,25 @@
+#include "vector.h"
+#include "ray.h"
+
+void	ray_at(t_vec *res, t_ray *ray, double t)
+{
+	vec_scale(res, t, &(ray->dir));
+	vec_add(res, &(ray->orig), res); 
+}
+
+void	ray_fillRay(t_ray *ray, t_vec orig, t_vec dir)
+{
+	ray->orig = orig;
+	ray->dir = dir;
+}
+
+int main()
+{
+	t_vec	orig;
+	t_vec	dir;
+	t_ray	ray;
+
+	vec_fillVec(&orig, 1, 1, 1);
+	vec_fillVec(&dir, 2, 2, 2);
+	ray_fillRay(&ray, orig, dir);
+}
---
+++ ray.h
@@ -0,0 +1,11 @@
+#ifndef RAY_H
+# define RAY_Y
+# include "vector.h"
+
+typedef struct s_ray
+{
+	t_vec	dir;
+	t_vec	orig;
+}	t_ray;
+
+#endif
