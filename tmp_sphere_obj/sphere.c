24873c85690784c5c8b3a6930256d21afd90adcb
charbel <khazencharbel@gmail.com>
2026-01-08 12:08:09 +0200

    fix hit function

--- tmp_sphere_obj/sphere.c
+++ tmp_sphere_obj/sphere.c
@@ -8,36 +8,39 @@ typedef struct s_sph_quadParams
 	double	a;
 	double	h;
 	double	c;
+	double	delta;
 }	t_sph_quadParams;
 
-static double	sph_getDelta(t_sph *sphere, t_ray *ray, t_sph_quadParams *params)
+static void	sph_getDelta(t_sph_quadParams *params, t_sph *sphere, t_ray *ray)
 {
 	t_vec	oc;
-	double	a;
-	double	h;
-	double	c;
 
 	oc = vec_subs(&sphere->center, &ray->orig);
-	a = vec_squaredLen(&ray->dir);
-	h = vec_dot(&ray->dir, &oc);
-	c = vec_vectorLen(&oc) - sphere->radius * sphere->radius;
-	return (h * h - a * c);
+	params->a = vec_squaredLen(&ray->dir);
+	params->h = vec_dot(&ray->dir, &oc);
+	params->c = vec_vectorLen(&oc) - sphere->radius * sphere->radius;
+	params->delta = (params->h * params->h - params->a * params->c);
 }
 
 int	sph_hit(t_sph *sphere, t_ray *ray, double t_min, double t_max, t_hitRec *rec)
 {
-	if (sph_getDelta(sphere, ray) < 0)
+	t_sph_quadParams	params;
+	t_vec	normal_non_unit;
+
+	sph_getDelta(&params, sphere, ray);
+	if (params.delta < 0)
 		return (0);
-	sqrtd = sqrt(discriminant);
-	root = (h - sqrtd) / a; // NEED TO COMP a
+	sqrtd = sqrt(params.discriminant);
+	root = (params.h - sqrtd) / params.a;
 	if (root <= t_min || root >= t_max)
 	{
-		root = (h + sqrtd) / a; //NEED h
+		root = (params.h + sqrtd) / params.a;
 		if (root <= t_min || root >= t_max)
 			return (0);
 	}
 	rec->t = root;
-	rec->p = ray_at(ray, rec->t);
-	rec->normal = v_div(v_sub(rec->p, sphere->center), sphere->radius); //wrong usage of functions
+	rec->p = ray_at(ray, root);
+	vec_subs(&normal_non_unit, &rec->p, &sphere->center);
+	vec_scale(&rec->normal, &normal_non_unit, 1 / sphere->radius);
 	return (1);
 }
