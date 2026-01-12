d1cb1facc6949a7cfbe645f07aaef802c6509a06
charbel <khazencharbel@gmail.com>
2026-01-12 17:37:36 +0200

    rm from object.h

--- object.h
+++ object.h
@@ -1,16 +1,6 @@
 #ifndef HIT_RECORD_H
 #define HIT_RECORD_H
 
-#include "vector.h"
-#include "material.h"
-
-typedef struct s_hitRec { //seperate it  (other module - intersection != object definition)
-	t_vec	p;
-	t_vec	normal; // NEED TO SEE HOW
-	t_mat	material;
-	double t;
-}	t_hitRec;
-
 typedef struct s_obj {
 	int	obj_shapeType;
 	void	*obj_shape;
