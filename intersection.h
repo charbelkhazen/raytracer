822960f3d56762ae2cb4e8fa88fe448c13af0866
charbel <khazencharbel@gmail.com>
2026-01-12 17:36:45 +0200

    adding intersection.h without further modif

---
+++ intersection.h
@@ -0,0 +1,14 @@
+#ifndef INTERSECTION_H
+#define INTERSECTION_H
+
+#include "vector.h"
+#include "material.h"
+
+typedef struct s_hitRec { //seperate it  (other module - intersection != object definition)
+	t_vec	p;
+	t_vec	normal; // NEED TO SEE HOW
+	t_mat	material;
+	double t;
+}	t_hitRec;
+
+#endif
