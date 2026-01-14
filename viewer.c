749e2cc0630ed174fb5bebf96a28ff966a84baee
charbel <khazencharbel@gmail.com>
2026-01-14 14:15:42 +0200

    add hfov in view struct

--- viewer.c
+++ viewer.c
@@ -1,8 +1,9 @@
 #include "viewer.h"
 
-void	viewer_fill(t_viewer *viewer, t_vec lookfrom, t_vec vup, t_vec lookat)
+void	viewer_fill(t_viewer *viewer, t_vec lookfrom, t_vec vup, t_vec lookat, double hfov)
 {
 	viewer->lookat = lookat;
 	viewer->lookfrom = lookfrom;
 	viewer->vup = vup;
+	viewer->hfov = hfov;
 }
--- viewer.h
+++ viewer.h
@@ -7,8 +7,9 @@ typedef struct s_viewer
 	t_vec	lookfrom;
 	t_vec	lookat;
 	t_vec	vup; //vector that defines x and y on plane orth. to look_at dir
+	double	hfov;
 }	t_viewer;
 
-void	viewer_fill(t_viewer *viewer, t_vec lookfrom, t_vec vup, t_vec lookat);
+void	viewer_fill(t_viewer *viewer, t_vec lookfrom, t_vec vup, t_vec lookat, double hfov);
 
 #endif
