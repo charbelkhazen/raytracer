a8ae7af8b95fcb608da2a8e22dc4174c018d035b
charbel <khazencharbel@gmail.com>
2026-01-21 15:10:56 +0200

    draft for list manager

--- managespheres.c
+++ managespheres.c
@@ -1,9 +1,22 @@
 #include "managespheres.h"
 #include "sphere.h"
+#include "stdlib.h"
 
-void	mngsph_sphere_list_init(t_sphere_list *list, t_sph *buffer, int capacity)
+void	mngsph_sphere_list_init(t_sphere_list *list, t_sph *data, int capacity)
 {
-	list->data = buffer;
+	list->data = data;
 	list->capacity = capacity;
 	list->count = 0;
+}
+
+void	mngsph_new_sphere(t_sph *sphere, t_sphere_list *list)
+{
+	*sphere = list->data[(list->count)++];
+}
+
+void	mngsph_access_sphere_i(t_sph *sphere, t_sphere_list *list, int i)
+{
+	std_assert(i >= 0);
+
+	*sphere = list->data[i];
 }
--- managespheres.h
+++ managespheres.h
@@ -5,13 +5,20 @@
 
 typedef struct s_sphere_list
 {
+	//data represent the list of spheres (called "data" for naming coherence)
 	t_sph	*data;
 	int		capacity;
 	int		count;
 }	t_sphere_list;
 
-void	mngsph_sphere_list_init(t_sphere_list *list, t_sph *buffer, int capacity);
+//gets an initialized undefined t_sph *list_of_spheres and its capacity 
+void	mngsph_sphere_list_init(t_sphere_list *list, t_sph *list_of_spheres, int capacity);
 
-//void	mngsph_new_sphere(
+//get last t_sph sphere in sphere and count ++
+void	mngsph_new_sphere(t_sph *sphere, t_sphere_list *list);
+
+//t_sph *sphere will point to sphere i is list
+void	mngsph_access_sphere_i(t_sph *sphere, t_sphere_list list, int i);
+
 
 #endif
