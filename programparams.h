f09ab6127a876259cbed26b8ce14061751b6379e
charbel <khazencharbel@gmail.com>
2026-01-22 12:43:39 +0200

    move universe module to tmp

--- programparams.h
+++ programparams.h
@@ -1,12 +1,21 @@
 #ifndef PROGRAMPARAMS_H
 #define PROGRAMPARAMS_H
 
-typedef struct s_prog_params
+#include "light.h"
+#include "cam.h"
+#include "ambient.h"
+#include "managematerials.h"
+#include "managespheres.h"
+
+typedef struct s_rt_params
 {
 	t_light	light;
 	t_cam	cam;
 	t_ambientLight ambient;
 	t_material_list material;
 	t_sphere_list sphere;
-}
+	t_obj_list obj; // should be t_univ ?? 
+	// still  need to add cylinder and plane and different materials
+}	t_rt_params;
+
 #endif
---
+++ tmp_rm/cp_universe.h
@@ -0,0 +1,26 @@
+#ifndef UNIVERSE_H
+#define UNIVERSE_H
+#include "object.h"
+#include "ray.h"
+#include "vector.h"
+#include "universe.h"
+#include "intersection.h"
+
+typedef struct s_univ
+{
+	t_obj	*obj_lst; //if requires alloc, refactor to univ[100]
+	int	len;
+	int	cap;
+}	t_univ;
+
+void	univ_init(t_univ *univ, t_obj *empty_lst_objects, int lst_capacity);
+
+// by value not reference. manipulating object from univ will not change the orig obj
+int	univ_add(t_univ *univ, t_obj *object);
+
+void	univ_clear(t_univ *univ);
+
+//change name to hit ? or ishit?
+int	univ_hit(t_ray *ray, t_univ *univ, t_hitRec *rec); //does it make sense to have rec as input here -> review where exactly is rec used. see render
+
+#endif
