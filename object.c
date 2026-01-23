ce84fcc9159811aeaa6da9d88ac8b6ff05a050c2
charbel <khazencharbel@gmail.com>
2026-01-23 14:06:53 +0200

    mv original obj module to tmp , new obj module , DOES NOT COMPILE

--- object.c
+++ object.c
@@ -1,9 +1,31 @@
 #include "object.h"
+#include "vector.h"
+#include "sphere.h"
+//#include "matte.h"
 
-void	obj_fillObj(t_obj *obj, int obj_shapeType, void *obj_shape, int obj_matType, void *obj_mat)
+//need to add material after
+void	obj_fillObj(t_obj *obj, t_shape shape, t_vec color)
 {
-	obj->obj_shapeType = obj_shapeType;
-	obj->obj_shape = obj_shape;
-	obj->obj_matType = obj_matType;
-	obj->obj_mat = obj_mat;
+	obj->shape = shape;
+	obj->color = color;
 }
+
+/*
+#include <stdio.h>
+int main()
+{
+	t_obj obj;
+
+	t_sph sphere1;
+	t_vec	center1;
+
+	vec_fillVec(&center1, 0, 0, -5);
+	sph_fillSph(&sphere1, center1, 1);
+
+	t_vec color;
+
+	vec_fillVec(&color, 1, 1, 0);
+
+	obj_fillObj(&obj, sphere1, color);
+}
+*/
--- object.h
+++ object.h
@@ -1,12 +1,37 @@
-#ifndef HIT_RECORD_H
-#define HIT_RECORD_H
+#include "vector.h"
+#include "sphere.h"
+//#include "matte.h" // uncomment when fixed
 
-typedef struct s_obj {
-	int	obj_shapeType;
-	void	*obj_shape;
-	int	obj_matType;
-	void	*obj_mat;
+typedef struct s_shape
+{
+	int	type;
+	union
+	{
+		t_sph sphere;
+		//t_cyl cylinder;
+		//t_pl plane;
+	} as;
+}	t_shape;
+
+/*
+typedef struct s_material
+{
+	int	type;
+	union
+	{
+		t_mat matte;
+		//t_met metallic;
+		//t_gl  glass;
+	} as;
+}	t_material;
+*/
+
+typedef struct s_obj
+{
+	t_shape		shape;
+	//t_material	material;
+	t_vec		color;
 }	t_obj;
 
-void	obj_fillObj(t_obj *obj, int obj_shapeType, void *obj_shape, int obj_matType, void *obj_mat);
-#endif
+//need to add material after
+void	obj_fillObj(t_obj *obj, t_shape shape, t_vec color);
