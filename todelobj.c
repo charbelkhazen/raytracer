6466a1c91a12dc9fed37fb51d4d74b14c8f5a21f
charbel <khazencharbel@gmail.com>
2026-01-23 13:41:55 +0200

    added to todelobj.c - Step before changing todelobj.c to the actual object module and putting old module in tmp dir

--- todelobj.c
+++ todelobj.c
@@ -1,5 +1,7 @@
+//TODO: add t_material
 #include "sphere.h"
-#include "matte.h"
+
+//#include "matte.h"
 
 typedef struct s_shape
 {
@@ -12,6 +14,7 @@ typedef struct s_shape
 	} as;
 }	t_shape;
 
+/*
 typedef struct s_material
 {
 	int	type;
@@ -22,15 +25,37 @@ typedef struct s_material
 		//t_gl  glass;
 	} as;
 }	t_material;
+*/
 
 typedef struct s_obj
 {
 	t_shape		shape;
-	t_material	material;
+	//t_material	material;
 	t_vec		color;
 }	t_obj;
+
+//need to add material after
+void	obj_fillObj(t_obj *obj, t_shape shape, t_vec color)
+{
+	obj->shape = shape;
+	obj->color = color;
+}
+
+
 #include <stdio.h>
 int main()
 {
-	t_obj ob;
+	t_obj obj;
+
+	t_sph sphere1;
+	t_vec	center1;
+
+	vec_fillVec(&center1, 0, 0, -5);
+	sph_fillSph(&sphere1, center1, 1);
+
+	t_vec color;
+
+	vec_fillVec(&color, 1, 1, 0);
+
+	obj_fillObj(&obj, sphere1, color);
 }
