f50fab570134bd72064e0baee33f649234ca78a5
charbel <khazencharbel@gmail.com>
2026-01-29 11:54:30 +0200

    fix tmp_parse not added to parser implementation

--- todelCHAT.c
+++ todelCHAT.c
@@ -1,7 +1,9 @@
-//#include "parser.h"
-#include <fcntl.h>
+here are more functions added to the parser implementation
+
+can you tell me if it has errors
+
+
 #include "get_next_line.h"
-// parse program
 
 //has two modes : check duplicates mode (mode 0) and check basic setup (mode 1)
 // on mode = 0 returns 1 if count light or camera or ambient > 1 and 0 otherwise
@@ -26,7 +28,7 @@ static int	pars_checkCounts(t_cmd_type type, int mode)
 	if (!mode)
 	{
 		if (count_light > 1 || count_camera > 1 || count_ambient > 1)
-			return (1);
+			return (1)
 		return (0);
 	}
 	//mode == 1 -> Basic setup completeness check
@@ -43,7 +45,7 @@ static void	pars_setDefaultImg(t_scene *scene)
 
 	img_ratio = 16.0 / 9.0;
 	img_width = 900;
-	img_fill(&scene->cam.img, img_width, img_ratio);
+	img_fill(&scene.cam.img, img_width, img_ratio);
 }
 
 int	pars_fillScene(t_scene *scene, t_parsables *parsables, t_cmd_type cmdtype)
@@ -51,10 +53,11 @@ int	pars_fillScene(t_scene *scene, t_parsables *parsables, t_cmd_type cmdtype)
 	if (cmdtype == CAMERA_CMD)
 	{
 		pars_setDefaultImg(scene);
-		cam_fillCam(&scene->cam, scene->cam.img, parsables->view);
+		cam_fillCam(&scene->cam, scene->img, parsables->view);
 	}
 	else if (cmdtype == SPHERE_CMD) // TODO : DO THE SAME WITH CYL AND PLANE
 	{
+		univ_init(&scene.univ);
 		univ_addObj(&scene.univ, parsables->obj);
 	}
 	else if (cmdtype == LIGHT_CMD)
@@ -66,56 +69,46 @@ int	pars_fillScene(t_scene *scene, t_parsables *parsables, t_cmd_type cmdtype)
 	return (0);
 }
 
-//TODO: REFACTOR IF YOU HAVE TIME responsibabilities are not clear : checking + filling + initialzing init + initializing img - All this in one fnuction
-//FUNCTION INITIALIZES UNIV BEFORE STARTING THE PARSING
 // Parses program and fills scene with all its field
 int	pars_parseProgram(int fd, t_scene *scene)
 {
 	t_parsables	parsables;
 	char		*line;
-	t_cmd_type	cmdtype;
+	t_cmd_type	cmd_type;
 
-	univ_init(&scene.univ);
 	line = get_next_line(fd);
 	while (line)
 	{
-		if (pars_parseLine(&parsables, line, &cmdtype))
-		{
-			free(line);
-			return (1);
-		}
+		pars_parseLine(&parsables, line, &cmdtype);
 		//checks if repeated light , cam or ambient
 		if (pars_checkCounts(cmdtype, 0))
-		{
-			free(line);
 			return (1);
-		}
-		if (pars_fillScene(scene, &parsables, cmdtype))
-		{
-			free(line);
-			return (1);
-		}
-		free(line);
+		pars_fillScene(scene, &parsables, cmdtype);
 		line = get_next_line(fd);
 	}
-	free(line);
 	//checks for there are at least 1 object, cam ,light, ambient
 	if (pars_checkCounts(0, 1))
 		return (1);
 	return (0);
 }
 
-#include <stdio.h>
-int main()
-{
-	int fd;
+which depends (among other things ) on the scene interface :
 
-	fd = open("minirt.rt", O_RDONLY);
-	char *line;
-	while(line = get_next_line(fd))
-	{
-		
-		printf("%s\n", line);
-	}
-	close(fd);
-}
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "camera.h"
+#include "universe.h"
+#include "light.h"
+#include "ambientlight.h"
+
+//univ refers to the "universe of objects"
+typedef struct s_scene
+{
+	t_cam		cam;
+	t_univ		univ;
+	t_light		light;
+	t_ambientLight ambient;
+}	t_scene;
+	
+#endif
