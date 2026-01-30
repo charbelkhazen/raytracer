c281344c0dc2994c7c13e2d7f690dfc01eea6cc2
charbel <khazencharbel@gmail.com>
2026-01-30 10:07:08 +0200

    change render_rayColor parameter. Pass scene as reference. Does commit

--- render.h
+++ render.h
@@ -8,6 +8,6 @@ void	render_logicToMlx(ui_mlxParams_t *params, t_scene *scene);
 
 //sould be in render!!!
 //temporary function - needs modif
-void	render_rayColor(t_vec *color, t_ray *ray, t_univ *univ, t_light *light); // hit record should  be part of ray? 
 
+void	render_rayColor(t_vec *color, t_ray *ray, t_scene *scene);
 #endif
