a709311238715f8f10a17787a6da96be8ade4f8c
charbel <khazencharbel@gmail.com>
2026-01-31 13:02:33 +0200

    add function signatures to cylinder interface

--- cylinder.h
+++ cylinder.h
@@ -8,5 +8,10 @@ typedef struct s_cylinder
 	double	diameter;
 	double	height;
 }	t_cylinder;
-	
+
+void	cyl_fillCyl(t_cylinder *cyl, t_vec center, t_vec normalized_axis, double diameter, double height);
+
+//returns bool AND if hit fills hitRec
+int	sph_hit(t_cylinder *cyl, t_ray *ray, double t_min, double t_max, t_hitRec *rec);
+
 #endif
