b82cef89c1219d2f41f674459f07937ad90ee9cd
charbel <khazencharbel@gmail.com>
2026-02-01 19:22:32 +0200

    added plane and cylinder hit function in shape.c implementation. Does compile

--- shape.c
+++ shape.c
@@ -15,7 +15,10 @@ int	shape_hit(t_shape *shape, t_ray *ray, double t_min, double t_max, t_hitRec *
 		return (sph_hit(&shape->as.sphere, ray, t_min, t_max, rec)); 
 
 	if (shape->type == PLANE_TYPE)
-		return (plane_hit(ray,&shape->as.plane, t_min, t_max, rec)); 
+		return (plane_hit(ray, &shape->as.plane, t_min, t_max, rec));  //TODO: swap the order of ray and plane parameters to make it consistent with other hit funcions
+	
+	if (shape->type == CYLINDER_TYPE)
+		return (cyl_hit(&shape->as.cylinder,ray,  t_min, t_max, rec)); 
 
 	return (0);
 }
