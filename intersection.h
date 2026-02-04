8fe2a4443df860b6c56e8d74de317baa4b202ae6
charbel <khazencharbel@gmail.com>
2026-02-04 13:37:32 +0200

    refactoring intersection module - rm unnecessary header

--- intersection.h
+++ intersection.h
@@ -2,7 +2,6 @@
 #define INTERSECTION_H
 
 #include "vector.h"
-#include "material.h"
 
 typedef struct s_hitRec { //seperate it  (other module - intersection != object definition)
 	t_vec	p;
---
+++ tmp_rm/tmp_lambertphong.c
@@ -0,0 +1,5 @@
+
+void	lp_shade(t_vec *color, t_hitRec *rec, t_ray *ray, t_scene *scene)
+{
+	if (rec->mat == 	
+}
