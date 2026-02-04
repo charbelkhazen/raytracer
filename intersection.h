f1e495520a4c31af83ab96fa8febda4682d9d1bf
charbel <khazencharbel@gmail.com>
2026-02-04 13:54:13 +0200

    does compile - refactored intersection module so that mat and shape has enum types

--- intersection.h
+++ intersection.h
@@ -2,13 +2,14 @@
 #define INTERSECTION_H
 
 #include "vector.h"
+#include "types.h"
 
 typedef struct s_hitRec { //seperate it  (other module - intersection != object definition)
 	t_vec	p;
 	t_vec	normal;
 	t_vec	color;
-	int	shape;
-	int	mat;
+	shape_type	shape;
+	material_type	mat;
 	double	t;
 }	t_hitRec;
 
